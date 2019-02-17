b9dbbd49a3d5d73798f3af4dbe582dd0abe10227
fhh200000 <1041154549@qq.com>
2019-02-17 11:21:35 +0800

    Update:Furthered connected blocks

--- Main/itworkspop.cpp
+++ Main/itworkspop.cpp
@@ -34,17 +34,19 @@ void ItWorksPop::initMap(int w,int h,int layer0[],int layer1[],int layer2[])
             que[tmp]->initAtlas(atlasp[layer0[tmp]],atlasp[layer1[tmp]],atlasp[layer2[tmp]],j,i);
             que[tmp]->setGeometry(j*48,i*48,48,48);
             que[tmp]->show();
-            if(layer0[tmp]==188)
+            if(layer0[tmp]>175)
             {
-                extrablock a={i*w+j,w,188,2};
+                extrablock a={i*w+j,w,layer0[tmp],2};
                 spec.push_back(a);
             }
         }
     }
     if(spec.size())
     {
-        printf("%d,%d,%d,%d\n",spec.data()[0].count,spec.data()[0].width,spec.data()[0].index,spec.data()[0].style);
-        this->reloadBlock(spec.data()[0],w);
+        for(std::vector<extrablock>::iterator iter = spec.begin();iter !=spec.end();iter++)
+        {
+            this->reloadBlock(*iter,w);
+        }
     }
     this->resize(48*w,48*h);
     this->ui->bg->hide();
@@ -60,17 +62,64 @@ void ItWorksPop::removeMap()
     que=nullptr;
     //this->ui->bg->show();
 }
-void ItWorksPop::reloadBlock(extrablock in,int width)
+void ItWorksPop::reloadBlock(extrablock in, int width)
 {
-    QPixmap * rock = (in.style==1)?QuellBlock::rockstyle1:QuellBlock::rockstyle2;
-    switch (in.index) {
+    QPixmap * rock = in.style==1?QuellBlock::rockstyle1:QuellBlock::rockstyle2;
+    switch (in.index)
+    {
+        case 176:
+        {
+            que[in.count]->reloadAtlas(rock[2]);
+            break;
+        }
+        case 177:
+        {
+            que[in.count]->reloadAtlas(rock[3]);
+            break;
+        }
+        case 178:
+        {
+            que[in.count]->reloadAtlas(rock[4]);
+            break;
+        }
+        case 179:
+        {
+            que[in.count]->reloadAtlas(rock[8]);
+            break;
+        }
         case 188:
         {
             que[in.count]->reloadAtlas(rock[5]);
             que[in.count+1]->reloadAtlas(rock[6]);
             que[in.count+width]->reloadAtlas(rock[13]);
             que[in.count+width+1]->reloadAtlas(rock[14]);
-        printf("%d\n",in.count);
+            break;
+        }
+        case 189:
+        {
+            que[in.count]->reloadAtlas(rock[7]);
+            que[in.count+width]->reloadAtlas(rock[15]);
+            que[in.count+width*2]->reloadAtlas(rock[23]);
+            break;
+        }
+        case 187:
+        {
+            que[in.count]->reloadAtlas(rock[43]);
+            que[in.count+1]->reloadAtlas(rock[44]);
+            break;
+        }
+        case 185:
+        {
+            que[in.count]->reloadAtlas(rock[16]);
+            que[in.count+width]->reloadAtlas(rock[24]);
+            break;
+        }
+        case 192:
+        {
+            que[in.count]->reloadAtlas(rock[25]);
+            que[in.count+width]->reloadAtlas(rock[33]);
+            que[in.count+width+1]->reloadAtlas(rock[34]);
+            break;
         }
     }
 }
