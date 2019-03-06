0e0b5965f59a7f5d481f9bfd5025191f664f442e
fhh200000 <1041154549@qq.com>
2019-03-06 12:19:28 +0800

    Update:Added name strings export function.

--- Main/exportaction.cpp
+++ Main/exportaction.cpp
@@ -1,8 +1,12 @@
-#include "exportaction.h"
+#include <cstdio>
+#include <libxml/parser.h>
+#include <cstring>
+#include <vector>
 #define MAX_PATH_LENGTH 128
 using namespace std;
 int saveinfo(void)
 {
+    char path[MAX_PATH_LENGTH]={0};
     FILE *fp;
     unsigned long count;
     fp = fopen("/home/fhh/桌面/tmp3/general.gmp","r");
@@ -49,66 +53,193 @@ int saveinfo(void)
     for(ulong i=0;i<lvl_zen_e.size();i++)
     {
         lvl_zen_e.data()[i] = cur;
-        printf("%s\n",xmlNodeGetContent(cur));
         cur = cur->next;
     }
-    printf("SUBROUTINE----------------------\n");
     for(ulong i=0;i<lvl_classic.size()-2;i++)
     {
         lvl_classic.data()[i] = cur;
-        printf("%s\n",xmlNodeGetContent(cur));
         cur = cur->next;
-        if(i==5)
+        if(i==4)
         {
             i++;
-            lvl_classic.data()[6]=nullptr;//Fixed a bug caused by FTG
+            lvl_classic.data()[5]=nullptr;//Fixed a bug caused by FTG
         }
     }
-    printf("SUBROUTINE----------------------\n");
     for(ulong i=0;i<lvl_d.size();i++)
     {
         lvl_d.data()[i] = cur;
-        printf("%s\n",xmlNodeGetContent(cur));
         cur = cur->next;
     }
-    printf("SUBROUTINE----------------------\n");
     for(ulong i=0;i<lvl_z.size();i++)
     {
         lvl_z.data()[i] = cur;
-        printf("%s\n",xmlNodeGetContent(cur));
         cur = cur->next;
     }
-    printf("SUBROUTINE----------------------\n");
     for(ulong i=0;i<lvl_g.size();i++)
     {
         lvl_g.data()[i] = cur;
-        printf("%s\n",xmlNodeGetContent(cur));
         cur = cur->next;
     }
-    printf("SUBROUTINE----------------------\n");
     for(ulong i=0;i<lvl_m.size();i++)
     {
         lvl_m.data()[i] = cur;
-        printf("%s\n",xmlNodeGetContent(cur));
         cur = cur->next;
     }
-    printf("SUBROUTINE----------------------\n");
     for(ulong i=0;i<lvl_b.size();i++)
     {
         lvl_b.data()[i] = cur;
-        printf("%s\n",xmlNodeGetContent(cur));
         cur = cur->next;
     }
-    printf("SUBROUTINE----------------------\n");
     for(ulong i=0;i<lvl_candy.size();i++)
     {
         lvl_candy.data()[i] = cur;
-        printf("%s\n",xmlNodeGetContent(cur));
         cur = cur->next;
     }
-    printf("SUBROUTINE----------------------\n");
     //This is our Magic Code!
     int classindex[] = {21,12,11,2,22,0,27,29,37,13,15,32,28,16,38,3,25,33,39,26,36,9,19,20,23,14,8,7,30,10,34,1,31,18,17,35,24,6,4};
+    //Then, we open the level xml
+    xmlDocPtr doclvl;
+    xmlNodePtr curlvl;
+    xmlKeepBlanksDefault(0);
+    doclvl = xmlParseFile("/home/fhh/桌面/levels_zen.txt");
+    curlvl = xmlDocGetRootElement(doclvl);
+    printf("Now loading level file......\n");
+    if(curlvl == nullptr)
+    {
+        fprintf(stderr, "root error!\n");
+        xmlFreeDoc(doc);
+        return 0;
+    }
+    if(xmlStrcmp(curlvl->name, reinterpret_cast<const xmlChar*>("serialise")))
+    {
+        printf("end\n");
+        return 0;
+    }
+    curlvl = curlvl->children;
+    //After that, we switch to the first level
+    while(curlvl->name[4]!='d')
+    {
+        curlvl = curlvl->next;
+    }
+    //Then Everything is OK
+    //Now We are prepared to load split files.
+    char name[32]={0};
+    char id[32]={0};
+    xmlNodePtr now=nullptr;
+    int tmp,needReload,posi,reloadindex=0;
+    for(int i=0;i<192;i++)
+    {
+        sprintf(path,"/home/fhh/桌面/tmp/%d.%d.%d.gmp",(i/16)+1,(i&12)/4+1,(i&3)+1);
+        fp = fopen(path,"r");
+        fscanf(fp,"[QuellGen level data file]\n");
+        fscanf(fp,"Level ID:%s\n",id);
+        //判断关卡类型
+        switch(id[4])
+        {
+            case 'd':
+            {
+                tmp = atoi(((id))+5);
+                if(!tmp)
+                {
+                    needReload=1;
+                    goto outswitch;
+                }
+                sscanf((id),"lvl_d%d\n",&posi);
+                now = lvl_d[static_cast<ulong>(posi)-1];
+                break;
+            }
+            case 'z':
+            {
+                tmp = atoi(((id))+5);
+                if(!tmp)
+                {
+                    needReload=1;
+                    goto outswitch;
+                }
+                sscanf((id),"lvl_z%d\n",&posi);
+                now = lvl_z[static_cast<ulong>(posi)-1];
+                break;
+            }
+            case 'g':
+            {
+                tmp = atoi(((id))+5);
+                if(!tmp)
+                {
+                    needReload=1;
+                    goto outswitch;
+                }
+                sscanf((id),"lvl_g%d\n",&posi);
+                now = lvl_g[static_cast<ulong>(posi)-1];
+                break;
+            }
+            case 'c':
+            {
+                tmp = (id[9]=='_');//select "candy"
+                    if(!tmp)
+                    {
+                        needReload=1;
+                        goto outswitch;
+                    }
+                sscanf((id),"lvl_candy_%d\n",&posi);
+                now = lvl_candy[static_cast<ulong>(posi)-1];
+                break;
+            }
+            case 'b':
+            {
+                tmp = atoi(((id))+5);
+                if(!tmp)
+                {
+                    needReload=1;
+                    goto outswitch;
+                }
+                sscanf((id),"lvl_b%d\n",&posi);
+                now = lvl_b[static_cast<ulong>(posi)-1];
+                break;
+            }
+            case 'm':
+            {
+                tmp = atoi(((id))+5);
+                if(!tmp)
+                {
+                    needReload=1;
+                    goto outswitch;
+                }
+                sscanf((id),"lvl_m%d\n",&posi);
+                now = lvl_m[static_cast<ulong>(posi)-1];
+                break;
+            }
+            case 'q':
+            {
+                sscanf((id),"lvl_qm_e%d\n",&posi);
+                now = lvl_zen_e[static_cast<ulong>(posi)-1];
+                break;
+            }
+            default:
+            {
+                needReload=1;
+            }
+
+            outswitch:if(needReload)
+            {
+                //printf("%s:%s\n",path,(char*)cur->name);
+                //name = (char*)cur->name;
+                if(id[4]=='c'&&id[5]=='r')
+                {
+                    now = lvl_zen_e[0];
+                    break;
+                }
+                now = lvl_classic[static_cast<ulong>(classindex[reloadindex++])];
+            }
+        }
+        //-------------------------------
+        fscanf(fp,"Level name:%[^\n]",name);
+        printf("%d.%d.%d:%s\n",(i/16)+1,(i&12)/4+1,(i&3)+1,name);
+        xmlNodeSetContent(now->children->next->next,reinterpret_cast<unsigned char*>(name));
+        fscanf(fp,"\n");
+        fclose(fp);
+    }
+    xmlSaveFormatFile("/home/fhh/桌面/strings.3.xml",doc,1);
+    xmlFreeDoc(doc);
     /*
     vector<char*> lvl_zen_e = vector<char*>();
     vector<char*> lvl_classic = vector<char*>();
@@ -124,7 +255,6 @@ int saveinfo(void)
     FILE *fp = nullptr;
     char name[32] = {0};
     int tmp,needReload;
-    char path[MAX_PATH_LENGTH]={0};
     for(int i=0;i<=193;i++)
     {
         sprintf(path,filelocation,(i/16)+1,(i&12)/4+1,(i&3)+1);
